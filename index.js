ae4ceaf961fd33a3bcdbd7dea9499e345a88865e
Jack Lach <jack45k@gmail.com>
2022-12-28 00:30:45 -0500

    commit tree 5bf89fa95db2d274113ccd38bdf101f466e7b54d

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
+
+// const 
+
+ const arrayOfStringNames  = ["Jack", "Charles", "David"];
+ let eventName = "Halloween";
+
+function writeCards(arrayOfStringNames, eventName) {
+    const arrayOfCardsWritten = [];
+    for (let i = 0; i < arrayOfStringNames.length; i++) {
+            arrayOfCardsWritten.push(`Thank you, ${arrayOfStringNames[i]}, for the wonderful ${eventName} gift!`);
+         // console.log(`Thank you, ${arrayOfStringNames[i]}, for the wonderful ${eventName} gift!`);
+       // debugger;
+    }
+    //return arrayOfStringNames;\
+    return arrayOfCardsWritten;
+}
+
+writeCards(arrayOfStringNames, eventName);
+
+function countDown(positiveInt) {
+    let count = positiveInt 
+    while (count > -1) {
+    console.log(count);
+    count--;
+    }
+}
+
+countDown(10);
+
+
+
+
 // Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     debugger;
+// }
+
+
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//         debugger;
+//     }
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+
