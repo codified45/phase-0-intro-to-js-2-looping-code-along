94f4603ff5ba95f51478f0b82ba979321b366be6
Jack Lach <jack45k@gmail.com>
2023-01-23 18:21:59 -0500

    js 2 looping code along 1/2 tests passing

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+// }
+
+// const gifts = ['teddy bear', 'drone', 'doll'];
+
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     }
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+ 
+
+function writeCards(arrayOfNames, eventName) {
+    const arrayOfCreatedMessages = [];
+    for (let i = 0; i < arrayOfNames.length; i++) {
+        let message = `Thank you, ${arrayOfNames[i]}, for the wonderful ${eventName} gift!`;
+
+        arrayOfCreatedMessages.push(message);
+    }
+        return arrayOfCreatedMessages;
+}
+
+
